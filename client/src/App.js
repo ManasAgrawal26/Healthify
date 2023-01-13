cc6d41e6633fcd036090966a5c798dba28b2d356
Pranav <kadampranav2003@gmail.com>
2023-01-13 14:00:03 +0530

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//components
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-
+      
+      <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
